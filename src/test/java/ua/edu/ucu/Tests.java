422ad311745495b7835362a15fecb411536d070e
Denis <you@example.com>
2019-11-22 05:08:02 +0200

    Fix

--- src/test/java/ua/edu/ucu/Tests.java
+++ src/test/java/ua/edu/ucu/Tests.java
@@ -2,6 +2,9 @@ package ua.edu.ucu;
 
 import org.junit.Assert;
 import org.junit.Test;
+import ua.edu.ucu.smartarr.BaseArray;
+import ua.edu.ucu.smartarr.DistinctDecorator;
+import ua.edu.ucu.smartarr.SmartArray;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -11,5 +14,12 @@ public class Tests {
         Student student = new Student("Name", "Surname", 100,3);
 
         assertEquals (student.toString(), "Student{name=Name, surname=Surname, GPA=100.0, year=3}");
+    }
+    @Test
+    public void testDistinct(){
+        Object [] array = {new Integer(3), new Integer(3), new Integer(4)};
+        BaseArray baseArray = new BaseArray(array);
+        DistinctDecorator decorator = new DistinctDecorator(baseArray);
+        int k= 0;
     }
 }
